e7791c456f9c9568675e80fc3b2b0f37f02512a7
홍준성 <powerstar13@hanmail.net>
2022-04-20 04:41:15 +0900

    PersonRepository Test > NotFound 검증

--- src/test/java/study/webclient/msainfoservicetask/domain/model/PersonRepositoryTest.java
+++ src/test/java/study/webclient/msainfoservicetask/domain/model/PersonRepositoryTest.java
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.server.ServerWebInputException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 class PersonRepositoryTest {
@@ -52,5 +52,19 @@ class PersonRepositoryTest {
             })
             .as("홍준성 정보 조회 검증")
             .verifyComplete();
+    }
+
+    @Test
+    void findFirstByPersonName_notFound() {
+
+        String name = "없는이름";
+
+        Mono<Void> personVoid = personRepository.findFirstByPersonName(name)
+            .log()
+            .thenEmpty(s -> s.onError(new ServerWebInputException("이름으로 조회되는 정보가 없습니다.")));
+
+        StepVerifier.create(personVoid)
+            .expectError(ServerWebInputException.class)
+            .verify();
     }
 }
