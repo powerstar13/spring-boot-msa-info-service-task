9cc363d1af3128f9a5721cd6efb8a5d61987b2bb
홍준성 <powerstar13@hanmail.net>
2022-04-20 06:36:51 +0900

    PersonApplicationService > 이름으로 조회한 정보에서 직업 정보 찾기

---
+++ src/main/java/study/webclient/msainfoservicetask/application/person/PersonApplicationService.java
@@ -0,0 +1,31 @@
+package study.webclient.msainfoservicetask.application.person;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+import study.webclient.msainfoservicetask.application.person.response.PersonJobResponse;
+import study.webclient.msainfoservicetask.domain.model.Person;
+import study.webclient.msainfoservicetask.domain.model.PersonFindSpecification;
+
+@Service
+@RequiredArgsConstructor
+public class PersonApplicationService {
+
+    private final PersonFindSpecification personFindSpecification;
+
+    /**
+     * 이름으로 조회된 정보에서 직업 정보 찾기
+     * @param personName : 이름
+     * @return Mono<PersonJobResponse> : 직업 정보
+     */
+    public Mono<PersonJobResponse> findPersonJobByPersonName(String personName) {
+
+        Mono<Person> personMono = personFindSpecification.findFirstByPersonName(personName); // 이름으로 정보 조회 검증
+
+        return personMono.flatMap(person -> Mono.just(
+            PersonJobResponse.builder()
+                .job(person.getPersonJob()) // 직업 정보 반영
+                .build()
+        ));
+    }
+}
