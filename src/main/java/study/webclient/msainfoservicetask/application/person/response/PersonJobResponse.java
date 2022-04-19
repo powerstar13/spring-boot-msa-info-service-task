8b6a8365b20f052bd3ae44bf96e312806eb1a48f
홍준성 <powerstar13@hanmail.net>
2022-04-20 06:35:42 +0900

    PersonJob Response > 직업 정보를 담을 반환 그릇

---
+++ src/main/java/study/webclient/msainfoservicetask/application/person/response/PersonJobResponse.java
@@ -0,0 +1,13 @@
+package study.webclient.msainfoservicetask.application.person.response;
+
+import lombok.*;
+
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonJobResponse {
+
+    private String job; // 직업 정보
+}
