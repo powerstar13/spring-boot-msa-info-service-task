3ead4b78db52e0b4d7a95403a3469149acaa0bed
홍준성 <powerstar13@hanmail.net>
2022-04-20 06:34:57 +0900

    Person 명세서 > 이름으로 정보 조회 검증

---
+++ src/main/java/study/webclient/msainfoservicetask/domain/model/PersonFindSpecification.java
@@ -0,0 +1,26 @@
+package study.webclient.msainfoservicetask.domain.model;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebInputException;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class PersonFindSpecification {
+
+    private final PersonRepository personRepository;
+
+    /**
+     * 이름으로 정보 검증
+     * @param personName : 이름
+     * @return Mono<Person> : 조회된 정보
+     */
+    public Mono<Person> findFirstByPersonName(String personName) {
+
+        Mono<Person> fallback = Mono.error(new ServerWebInputException(personName + "이름으로 조회되는 정보가 없습니다.")); // 조회된 정보가 없을 경우 fallback할 error 처리
+
+        return personRepository.findFirstByPersonName(personName)
+            .switchIfEmpty(fallback);
+    }
+}
